8515007d9e33cec81057b6f639b623b8201290ff
Nathan Kluth <nlkluth@gmail.com>
2015-06-19 12:44:04 -0600

    trying to resolve cwd issues

--- tasks/ec2_setup.js
+++ tasks/ec2_setup.js
@@ -61,7 +61,7 @@ module.exports = function (grunt) {
         ], [ // pm2
             'sudo apt-get install make g++ -y',
             'sudo npm install -g pm2 --unsafe-perm',
-            'sudo pm2 startup ubuntu'
+            'sudo pm2 startup amazon'
         ]];
 
         function forwardPort(from, to) {
